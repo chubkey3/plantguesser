e6bc187b1891be3ea3da574f582c266d78fc8ff6
Jason Louie <jason.jw.louie@gmail.com>
2022-04-24 19:31:58 -0700

    third commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <h1>Plant Guesser</h1>
       </div>
       <Imgdisplay></Imgdisplay>
-      <a href='https://github.com/ravager6969/flowerguesser' className='github' target='_blank' rel='noreferrer'><i className='fa fa-github' style={{fontSize: "24px"}}></i></a>
+      <a href='https://github.com/ravager6969/plantguesser' className='github' target='_blank' rel='noreferrer'><i className='fa fa-github' style={{fontSize: "24px"}}></i></a>
     </div>
   );
 }
